7852cae7b1b00b5ddc81ed492e65de999feaaeff
C.W. Betts <computers57@hotmail.com>
2016-04-26 14:58:36 -0600

    Cleaning up TextureLoading+OSX.swift

--- Xcode/SwiftLugaru/TextureLoading+OSX.swift
+++ Xcode/SwiftLugaru/TextureLoading+OSX.swift
@@ -13,7 +13,6 @@ import OpenGL.GL.Ext
 
 
 func loadTexture(fileURL: NSURL, inout textureID: GLuint, mipmap: Int32, hasAlpha: Bool) {
-	
 	guard let sourcefile = NSImage(contentsOfURL:fileURL) else {
 		return
 	}
@@ -22,54 +21,43 @@ func loadTexture(fileURL: NSURL, inout textureID: GLuint, mipmap: Int32, hasAlph
 		return
 	}
 	
-	
-	//LOGFUNC;
-	
 	print("Loading texture... " + fileURL.path!)
 	
-	//Is it valid?
 	do {
-		let		type: GLint
+		let type: GLint
 		//Alpha channel?
-		if hasAlpha {
+		if !imgRep.alpha {
 			type = GL_RGB
 		} else {
 			type = GL_RGBA
 		}
 		
-		glPixelStorei(GLenum(GL_UNPACK_ALIGNMENT), 1 );
+		glPixelStorei(GLenum(GL_UNPACK_ALIGNMENT), 1)
 		
 		if textureID == 0 {
-			glGenTextures( 1, &textureID );
+			glGenTextures(1, &textureID)
 		}
-		glTexEnvi(GLenum(GL_TEXTURE_ENV), GLenum(GL_TEXTURE_ENV_MODE), GL_MODULATE );
+		glTexEnvi(GLenum(GL_TEXTURE_ENV), GLenum(GL_TEXTURE_ENV_MODE), GL_MODULATE)
 		
 		glTexParameteri(GLenum(GL_TEXTURE_2D), GLenum(GL_TEXTURE_WRAP_S), GL_REPEAT)
 		glTexParameteri(GLenum(GL_TEXTURE_2D), GLenum(GL_TEXTURE_WRAP_T), GL_REPEAT)
-		glBindTexture(GLenum(GL_TEXTURE_2D), textureID);
-		glTexParameteri(GLenum(GL_TEXTURE_2D), GLenum(GL_TEXTURE_MAG_FILTER), GL_LINEAR );
+		glBindTexture(GLenum(GL_TEXTURE_2D), textureID)
+		glTexParameteri(GLenum(GL_TEXTURE_2D), GLenum(GL_TEXTURE_MAG_FILTER), GL_LINEAR)
 		if preferences.trilinearFiltering {
 			if mipmap != 0 {
-				glTexParameteri(GLenum(GL_TEXTURE_2D), GLenum(GL_TEXTURE_MIN_FILTER), GL_LINEAR_MIPMAP_LINEAR );
+				glTexParameteri(GLenum(GL_TEXTURE_2D), GLenum(GL_TEXTURE_MIN_FILTER), GL_LINEAR_MIPMAP_LINEAR)
 			}
 		}
 		if !preferences.trilinearFiltering {
 			if mipmap == 0 {
-				glTexParameteri(GLenum(GL_TEXTURE_2D), GLenum(GL_TEXTURE_MIN_FILTER), GL_LINEAR_MIPMAP_NEAREST );
+				glTexParameteri(GLenum(GL_TEXTURE_2D), GLenum(GL_TEXTURE_MIN_FILTER), GL_LINEAR_MIPMAP_NEAREST)
 			}
 		}
 		if mipmap == 0 {
-			glTexParameteri(GLenum(GL_TEXTURE_2D), GLenum(GL_TEXTURE_MIN_FILTER), GL_LINEAR );
+			glTexParameteri(GLenum(GL_TEXTURE_2D), GLenum(GL_TEXTURE_MIN_FILTER), GL_LINEAR)
 		}
-		
-		//glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, texture.sizeX, texture.sizeY, 0,
-		//          GL_BGRA_EXT, GL_UNSIGNED_INT_8_8_8_8_REV, texture.data);
-		
-		//gluBuild2DMipmaps( GL_TEXTURE_2D, type, texture.sizeX, texture.sizeY, GL_BGRA_EXT, GL_UNSIGNED_INT_8_8_8_8_REV, texture.data );
 		
 		glTexParameteri(GLenum(GL_TEXTURE_2D), GLenum(GL_GENERATE_MIPMAP), GL_TRUE)
 		glTexImage2D(GLenum(GL_TEXTURE_2D), 0, type, GLsizei(imgRep.pixelsWide), GLsizei(imgRep.pixelsHigh), 0, GLenum(imgRep.alpha ? GL_RGBA : GL_RGB), GLenum(GL_UNSIGNED_BYTE), imgRep.bitmapData)
-		//gluBuild2DMipmaps( GLenum(GL_TEXTURE_2D), type, GLsizei(imgRep.pixelsWide), GLsizei(imgRep.pixelsHigh), GLenum(imgRep.alpha ? GL_RGBA : GL_RGB), GLenum(GL_UNSIGNED_BYTE), imgRep.bitmapData );
-		
 	}
 }
